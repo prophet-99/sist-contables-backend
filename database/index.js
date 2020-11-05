b01c55660ba44f0c6b9160b77e8af068bd486b85
Alex Av <alexanderavilab@gmail.com>
2020-11-05 01:29:09 -0500

    te quiero x2 ☺♥, no te olvides de los register :D

--- database/index.js
+++ database/index.js
@@ -9,12 +9,8 @@ const proveedor = require('./../models/proveedor');
 const inventario = require('./../models/inventario');
 const compra = require('./../models/compra');
 const nomina = require('./../models/nomina');
-<<<<<<< HEAD
 const venta = require('./../models/venta');
-=======
 const estadoFinanciero = require('./../models/estado-financiero');
-const venta = require('../controllers/venta');
->>>>>>> 5ff06ffd678810f4a1bd31769f4f86b49ff5b460
 
 class MySQLServerConnection {
     static instance;
